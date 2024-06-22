bf4c7f17dee908a8e7e57975d345011991717973
Diontae <diontaelow@gmail.com>
2024-06-23 02:26:37 +0800

    fixed SQL queries

---
+++ SQL Scripts/createAccount.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Users (
+    id INT PRIMARY KEY IDENTITY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    role VARCHAR(30) NOT NULL CHECK (role IN ('Organisation', 'Admin', 'NormalUser'))
+);
+
+CREATE TABLE Admin (
+	id INT PRIMARY KEY IDENTITY,
+	user_id INT FOREIGN KEY REFERENCES Users(id),
+	username VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Organisation ( 
+	id INT PRIMARY KEY IDENTITY,
+	user_id INT FOREIGN KEY REFERENCES Users(id),
+	username VARCHAR(255) NOT NULL UNIQUE,	
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	orgNumber INT NOT NULL UNIQUE, /* ignoring the international code (+65), can? */
+);
+
+CREATE TABLE NormalUser (
+	id INT PRIMARY KEY IDENTITY,
+	user_id INT FOREIGN KEY REFERENCES Users(id),
+	username VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE
+);
