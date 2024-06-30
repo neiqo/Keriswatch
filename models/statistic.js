ea4c17891c1d809f153925b4936809ffac335179
SianKim <siankim06@gmail.com>
2024-06-30 17:42:18 +0800

    fixed minor bugs

--- models/statistic.js
+++ models/statistic.js
@@ -2,48 +2,48 @@ const sql = require("mssql");
 const dbConfig = require("../dbConfig");
 
 class Statistic {
-    constructor(statisticsID, country, category, year, percentage) {
-      this.statisticsID = statisticsID;
-      this.country = country;
-      this.category = category;
-      this.year = year;
-      this.percentage = percentage;
-    }
+  constructor(statisticsID, country, category, year, percentage) {
+    this.statisticsID = statisticsID;
+    this.country = country;
+    this.category = category;
+    this.year = year;
+    this.percentage = percentage;
+  }
 
-    static async getAllStatistics() {
-        const connection = await sql.connect(dbConfig);
+  static async getAllStatistics() {
+    const connection = await sql.connect(dbConfig);
     
-        const sqlQuery = `SELECT * FROM CountryStatistics`;
+    const sqlQuery = `SELECT * FROM CountryStatistics`;
     
-        const request = connection.request();
-        const result = await request.query(sqlQuery);
+    const request = connection.request();
+    const result = await request.query(sqlQuery);
     
-        connection.close();
+    connection.close();
     
-        return result.recordset.map(
-          (row) => new Statistic(row.statisticsID, row.country, row.category, row.year, row.percentage)
-        );
-    }
+    return result.recordset.map(
+      (row) => new Statistic(row.statisticsID, row.country, row.category, row.year, row.percentage)
+    );
+  }
 
-    static async getStatisticsByCountry(country) {
-        const connection = await sql.connect(dbConfig);
+  static async getStatisticsByCountry(country) {
+    const connection = await sql.connect(dbConfig);
     
-        const sqlQuery = `SELECT * FROM CountryStatistics WHERE country = @country AND year = '2022'`; // Parameterized query
+    const sqlQuery = `SELECT * FROM CountryStatistics WHERE country = @country AND year = '2022'`; // Parameterized query
     
-        const request = connection.request();
-        request.input('country', sql.VarChar, country);
-        const result = await request.query(sqlQuery);
+    const request = connection.request();
+    request.input('country', sql.VarChar, country);
+    const result = await request.query(sqlQuery);
     
-        connection.close();
+    connection.close();
     
-        return result.recordset.map((row) => new Statistic(
-            row.statisticsID,
-            row.country,
-            row.category,
-            row.year,
-            row.percentage
-          ));
-    }
+    return result.recordset.map((row) => new Statistic(
+      row.statisticsID,
+      row.country,
+      row.category,
+      row.year,
+      row.percentage
+    ));
+  }
 }
 
 module.exports = Statistic;
