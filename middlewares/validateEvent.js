39b49a9ca7b81f693d9a13a29bbfc6a25c9d1810
Vincent <s10257433@connect.np.edu.sg>
2024-06-30 19:48:25 +0800

    Improve posting events validation

--- middlewares/validateEvent.js
+++ middlewares/validateEvent.js
@@ -1,4 +1,6 @@
 const Joi = require("joi");
+const path = require("path");
+const fs = require("fs");
 
 const validateEvent = (req, res, next) => {
     const schema = Joi.object({
@@ -13,20 +15,43 @@ const validateEvent = (req, res, next) => {
     //enddate must be later than startdate
     const validation = schema.validate(req.body, { abortEarly: false }); // Validate request body
   
+    const startDate = new Date(req.body.startDate);
+    const endDate = new Date(req.body.endDate);
+    const today = new Date();
+
     if (validation.error) {
       const errors = validation.error.details.map((error) => error.message);
       res.status(400).json({ message: "Validation error", errors });
+      // Remove the uploaded file
+      fs.unlinkSync(req.file.path);
       return; // Terminate middleware execution on validation error
+    }  
+    else if (startDate < today) {
+      res.status(400).json({ message: "Start date must be later than today." });
+      // Remove the uploaded file
+      fs.unlinkSync(req.file.path);
+      return;
     } 
-    else if (startDate < new Date().toISOString().split('T')[0]) {
-        res.status(400).json({ message: "Start date must be later than today." });
-        return;
-    }
     else if (endDate < startDate) {
-        res.status(400).json({ message: "End date must be later than start date." });
-        return;
-  
+      res.status(400).json({ message: "End date must be later than start date." });
+      // Remove the uploaded file
+      fs.unlinkSync(req.file.path);
+      return;
     }
+    //This will replace the json of displaying event.
+    // else {
+    //     res.status(201).json({
+    //         message: "Event successfully created."
+    //     })
+    // }
+  
+    if (req.file) {
+      req.body.imagePath = path.join("/images/events", req.file.originalname);
+      console.log("File path set in validateEvent", path.join(__dirname, "../public/images/events", req.file.originalname));
+    } else {
+      return res.status(400).json({ message: "Image is required." });
+    }
+
     next(); // If validation passes, proceed to the next route handler
   };
   
