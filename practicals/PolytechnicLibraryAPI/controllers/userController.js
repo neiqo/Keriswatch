07723760a48115d9dc27f1d1816552f821338b5c
neiqo <zaquadq@gmail.com>
2024-07-04 02:23:31 +0800

    Complete Part 3

--- practicals/PolytechnicLibraryAPI/controllers/userController.js
+++ practicals/PolytechnicLibraryAPI/controllers/userController.js
@@ -39,6 +39,44 @@ const registerUser = async (req, res) => {
   }
 };
 
+const userLogin = async (req,res) => {
+  const {username, password} = req.body;
+
+  try {
+    // check if user is in the database based on unique username
+    const user = await User.getUserByUsername(username);
+    if (!user) {
+      return res.status(401).json({message: "User not found!"});
+    }
+
+    // check if password used to login matches the password in the database
+    //  - the login password is hashed using bcrypt's algorithm and checks if the
+    //    login password that is hashed matches exactly to the hashed password in the database
+    const isMatchedPass = await bcrypt.compare(password, user.passwordHash);
+    if (!isMatchedPass){
+      return res.status(401).json({message: "Wrong password!"});
+    }
+
+    // generate JWT(JSON Web Token) token
+    // user information that is pulled from the database
+    const payload = {
+      id: user.id,
+      role: user.role,
+    };
+    
+    // JWT token that expires after 1 hour that signed by the secret key
+    // server will verify the signature/token using the secret key
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "3600s"});
+
+    return res.status(200).json({ token }); // returns the JWT token
+  }
+  catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" }) 
+  }
+}
+
 module.exports = {
   registerUser,
+  userLogin
 };
