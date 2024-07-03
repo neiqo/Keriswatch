e568b22258f67ec6a6fe67d67dc14fc3599005fc
SianKim <siankim06@gmail.com>
2024-07-03 10:50:25 +0800

    updated sql

--- practicals/PolytechnicLibraryAPI/sql_scripts/create.sql
+++ practicals/PolytechnicLibraryAPI/sql_scripts/create.sql
@@ -1,6 +1,6 @@
 -- Create Users Table
 CREATE TABLE Users (
-    user_id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY IDENTITY(1,1),
     username VARCHAR(255) UNIQUE NOT NULL,
     passwordHash VARCHAR(255) NOT NULL,
     role VARCHAR(20) CHECK (role IN ('member', 'librarian')) NOT NULL
